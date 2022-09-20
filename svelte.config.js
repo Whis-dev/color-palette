2c66ea262f391a73391407f908630fd5bb991f0c
whis <whisdevj@gmail.com>
2022-09-21 00:33:25 +0900

    [env] - delete comment

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-// const dev = process.env.NODE_ENV === 'development';
-
-// console.log(process.env)
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
