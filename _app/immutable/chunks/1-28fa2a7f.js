df2779eb4aaf80cfd5e4f2abfcea57546b6ebafc
Whis-dev <whisdevj@gmail.com>
2022-09-20 14:56:59 +0000

    Deploying to gh-pages from @ Whis-dev/color-palette@09c6586192e91acdcfa742386cf17e3b6a110073 🚀

--- _app/immutable/chunks/1-28fa2a7f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-606d01c6.js";export{t as component};
---
+++ _app/immutable/chunks/1-2e931cfb.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-8e4bbec2.js";export{t as component};
